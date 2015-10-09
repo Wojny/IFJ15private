18814856b522ef19a959e2bfc476e217b80cf5bc
polakdavid <polak-david@hotmail.com>
2015-10-09 19:23:29 +0200

    Create errors.h

---
+++ errors.h
@@ -0,0 +1,19 @@
+//+------------------------------------------------------------------+
+//|	Project name:        IFJ - Team project                          |
+//| Filename:            errors.h                                    |
+//| Author:                                                          |
+//|	Encoding:            UTF-8							                  			 |
+//|	Description:         Definition of error codes.		        			 |
+//+------------------------------------------------------------------+
+
+#define IFJ_ERR_LEXICAL 1
+#define	IFJ_ERR_SYNTAX 2
+#define	IFF_ERR_UNREDEF 3
+#define	IFJ_ERR_MISSPARAM 4
+#define	IFJ_ERR_WRONGVARIABLE 5
+#define	IFJ_ERR_OTHER_SEMANTIC 6
+#define	IFJ_ERR_RUNTIME_INT 7
+#define	IFJ_ERR_RETYPE 8
+#define	IFJ_ERR_DIV_ZERO 9
+#define	IFJ_ERR_OTHER_RUNTIME 10
+#define	IFJ_ERR_INTERNAL 99
