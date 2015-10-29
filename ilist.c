e630bb8a2b162732820fb9d88d6366c3261793cb
Wojny <Wojny@users.noreply.github.com>
2015-10-29 22:00:27 +0100

    Create ilist.c

---
+++ ilist.c
@@ -0,0 +1,67 @@
+/* Částečně převzato z ukázkového Jednochuché interpretu - upravit a okomentovat*/
+
+#include <stdio.h>
+#include <malloc.h>
+#include "ilist.h"
+
+void listInit(tList *L)
+{
+  L->first  = NULL;
+  L->last   = NULL;
+  L->active = NULL;
+}
+
+void listFree(tList *L)
+{
+  tItemList *ptr;
+  while (L->first != NULL)
+  {
+    ptr = L->first;
+    L->first = L->first->nextItem;
+    free(ptr);
+  }
+}
+
+void listInsertLast(tList *L, tInstr I)
+{
+  tItemList *newItem;
+  newItem = malloc(sizeof (tItemList));
+  newItem->Instruction = I;
+  newItem->nextItem = NULL;
+  if (L->first == NULL)
+     L->first = newItem;
+  else
+     L->last->nextItem=newItem;
+  L->last=newItem;
+}
+
+void listFirst(tList *L)
+{
+  L->active = L->first;
+}
+
+void listNext(tList *L)
+{
+  if (L->active != NULL)
+  L->active = L->active->nextItem;
+}
+
+void listGoto(tList *L, void *gotoInstr)
+{
+  L->active = (tItemList*) gotoInstr;
+}
+
+void *listGetPointerLast(tList *L)
+{
+  return (void*) L->last;
+}
+
+tInstr *listGetData(tList *L)
+{
+  if (L->active == NULL)
+  {
+    printf("Chyba, zadna instrukce neni aktivni");
+    return NULL;
+  }
+  else return &(L->active->instr);
+}
